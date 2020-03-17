79930012cfa00135b7261552ab2f0607ccec4792
QL1998 <liqiaoyan1115@gmail.com>
2020-03-17 17:14:23 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnGitDemo
+//
+//  Created by AnoMac on 2020/3/17.
+//  Copyright © 2020 AnoMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
